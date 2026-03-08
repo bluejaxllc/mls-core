b3ae622438c59ceab75b09b68d29f62bdc12fc4b
BlueJax Deploy <deploy@bluejax.ai>
2026-03-08 17:06:26 -0600

    auto: update do_login.bat

---
+++ proxy/do_login.bat
@@ -0,0 +1,4 @@
+@echo off
+set HOMEPATH=\Users\edgar
+set USERPROFILE=C:\Users\edgar
+"C:\Users\edgar\.cloudflared\cloudflared.exe" tunnel login > "C:\Users\edgar\OneDrive\Desktop\AI APPS\MLS\proxy\fresh_login.log" 2>&1
